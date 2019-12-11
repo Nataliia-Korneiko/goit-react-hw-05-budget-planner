cd959562e163c3124dbd42de16a8b8e04e232777
Nataliia Korneiko <korneyko_ns@ukr.net>
2019-12-11 17:49:12 +0200

    Updates

---
+++ precache-manifest.d3b77b5f7a5c2f2b04b2c2a993aadb16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e795664ae61e8825a000e572c0f82c",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "d32bbe60b5c713070877",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "3df5110eb0ab9021d190",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.b7ff7796.chunk.js"
+  },
+  {
+    "revision": "d32bbe60b5c713070877",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.fe203a84.chunk.js"
+  },
+  {
+    "revision": "05df1f71193a6b1ae3a5",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.17532924.js"
+  }
+]);
